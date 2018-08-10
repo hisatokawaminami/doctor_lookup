b110ccd96804890ebe9bcdc75ef8a829f983cd94
Guest User <Guest@epicodus-50.local>
2018-08-10 16:09:13 -0700

    add accepting new patient function

--- src/doctor-interface.js
+++ src/doctor-interface.js
@@ -17,12 +17,15 @@ $(document).ready(function(){
       let foundCounter = 0;
       // let j = doctor.data.practices.phones.length;
       for (let i = 0; i < doctor.data.length; i++) {
-        $('.search').append("<li>" + `${doctor.data[i].profile.first_name}  ${doctor.data[i].profile.last_name}` + "</li>" + "<ul>" + `Address:  ${doctor.data[i].practices[0].visit_address.street}, ${doctor.data[i].practices[0].visit_address.city}` + "</ul>" + "<ul>" + `Phone: ${doctor.data[i].practices[0].phones[0].number}` + "</ul>" + "<ul>" + `Website: ` + `<a href="${doctor.data[i].practices[0].website}">` + `${doctor.data[i].practices[0].website}` + "</a>" +  "</ul>");
-
+        if (`${doctor.data[i].practices[0].accepts_new_patients}` === "true" ) {
+            $('.search').append("<li>" + `${doctor.data[i].profile.first_name}  ${doctor.data[i].profile.last_name}` + "</li>" + "<ul>" + `Address:  ${doctor.data[i].practices[0].visit_address.street}, ${doctor.data[i].practices[0].visit_address.city}` + "</ul>" + "<ul>" + `Phone: ${doctor.data[i].practices[0].phones[0].number}` + "</ul>" + "<ul>" + `Website: ` + `<a href="${doctor.data[i].practices[0].website}">` + `${doctor.data[i].practices[0].website}` + "</a>" +  "</ul>" + "<ul>" + "Accepting New Patients: Yes" + "</ul<");
+        } else {
+        $('.search').append("<li>" + `${doctor.data[i].profile.first_name}  ${doctor.data[i].profile.last_name}` + "</li>" + "<ul>" + `Address:  ${doctor.data[i].practices[0].visit_address.street}, ${doctor.data[i].practices[0].visit_address.city}` + "</ul>" + "<ul>" + `Phone: ${doctor.data[i].practices[0].phones[0].number}` + "</ul>" + "<ul>" + `Website: ` + `<a href="${doctor.data[i].practices[0].website}">` + `${doctor.data[i].practices[0].website}` + "</a>" +  "</ul>" + "<ul>" +"Accepting New Patients: No" + "</ul<");
+      }
         foundCounter++;
       }
       if (foundCounter === 0){
-        $('.showCounter').text(foundCounter + ' found');
+        $('.showCounter').text(foundCounter + ' found: NO DOCTOR MEET CRITERIA');
       } else {
         $('.showCounter').text(foundCounter + ' showing');
       }
