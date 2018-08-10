3c39a7b5fecf8ae7b6af83c54de58eb82637a6ef
Guest User <Guest@epicodus-50.local>
2018-08-10 12:10:25 -0700

    add 0 found if there is no match in search

--- src/doctor-interface.js
+++ src/doctor-interface.js
@@ -6,6 +6,8 @@ import { DoctorLookup } from './../src/doctor.js';
 $(document).ready(function(){
   let page = 0;
   $('#symptonSearch').click(function(){
+    $('.search').text("");
+
     let sympton = $('#sympton').val();
     let doctorLookup = new DoctorLookup();
     let symptonPromise = doctorLookup.findDocBySympton(sympton, page);
@@ -17,7 +19,13 @@ $(document).ready(function(){
           $('.search').append("<li>" + `${doctor.data[i].profile.first_name}  ${doctor.data[i].profile.last_name}` + "</li>");
           foundCounter++;
       }
-      $('.showCounter').text(foundCounter + ' showing');
+      if (foundCounter === 0){
+        $('.showCounter').text(foundCounter + ' found');
+
+      } else {
+        $('.showCounter').text(foundCounter + ' showing');
+
+      }
 
     }, function(error) {
       $('.search').text(error.message);
