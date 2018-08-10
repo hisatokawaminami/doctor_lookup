9503e7b14ad2f4c22068df85331828fefc148013
Guest User <Guest@epicodus-50.local>
2018-08-10 10:48:49 -0700

    add function to search a doctor by sympton

--- src/doctor-interface.js
+++ src/doctor-interface.js
@@ -6,6 +6,14 @@ import { DoctorLookup } from './../src/doctor.js';
 $(document).ready(function(){
   $('#symptonSearch').click(function(){
     let sympton = $('#sympton').val();
-    let
+    let doctorLookup = new DoctorLookup();
+    let symptonPromise = doctorLookup.findDocBySympton(sympton);
+
+    symptonPromise.then(function(response){
+      let doctor = JSON.parse(response);
+      $('.search').append(doctor.data[0].practices[0].name);
+    }, function(error) {
+      $('.search').text(error.message);
+    });
   });
 });
--- src/doctor.js
+++ src/doctor.js
@@ -1,5 +1,20 @@
 export class DoctorLookup{
-  findDocBySympton(){
+  findDocBySympton(sympton){
+    return new Promise(function(resolve, reject){
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${sympton}&location=wa-seattle&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
 
+    
+      request.onload = function(){
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
   }
+
 }
