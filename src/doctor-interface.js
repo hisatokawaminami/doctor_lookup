2148d503526cc87cc915c586db46d493b0a3a3df
Guest User <Guest@epicodus-50.local>
2018-08-10 16:46:17 -0700

    add function that searches both name and sympton

--- src/doctor-interface.js
+++ src/doctor-interface.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
     let name = $('#name').val();
     let sympton = $('#sympton').val();
     let doctorLookup = new DoctorLookup();
-    let symptonPromise = doctorLookup.findDocBySympton(sympton, page);
+    let symptonPromise = doctorLookup.findDocBySympton(sympton, name, page);
 
     symptonPromise.then(function(response){
       let doctor = JSON.parse(response);
@@ -19,64 +19,35 @@ $(document).ready(function(){
       for (let i = 0; i < doctor.data.length; i++) {
         if (`${doctor.data[i].practices[0].accepts_new_patients}` === "true" ) {
           $('.search').append(
-              "<li>" + `${doctor.data[i].profile.first_name}  ${doctor.data[i].profile.last_name}` + "</li>" +
-              "<ul>" + `Address:  ${doctor.data[i].practices[0].visit_address.street}, ${doctor.data[i].practices[0].visit_address.city}` + "</ul>" +
-              "<ul>" + `Phone: ${doctor.data[i].practices[0].phones[0].number}` + "</ul>" +
-              "<ul>" + `Website: ` + `<a href="${doctor.data[i].practices[0].website}">` + `${doctor.data[i].practices[0].website}` + "</a>" +
-              "</ul>" +
-              "<ul>" + "Accepting New Patients: Yes" + "</ul<");
-        } else {
-          $('.search').append(
+            "<li>" + `${doctor.data[i].profile.first_name}  ${doctor.data[i].profile.last_name}` + "</li>" +
+            "<ul>" + `Address:  ${doctor.data[i].practices[0].visit_address.street}, ${doctor.data[i].practices[0].visit_address.city}` + "</ul>" +
+            "<ul>" + `Phone: ${doctor.data[i].practices[0].phones[0].number}` + "</ul>" +
+            "<ul>" + `Website: ` + `<a href="${doctor.data[i].practices[0].website}">` + `${doctor.data[i].practices[0].website}` + "</a>" +
+            "</ul>" +
+            "<ul>" + "Accepting New Patients: Yes" + "</ul<");
+          } else {
+            $('.search').append(
               "<li>" + `${doctor.data[i].profile.first_name}  ${doctor.data[i].profile.last_name}` + "</li>" +
               "<ul>" + `Address:  ${doctor.data[i].practices[0].visit_address.street}, ${doctor.data[i].practices[0].visit_address.city}` + "</ul>" +
               "<ul>" + `Phone: ${doctor.data[i].practices[0].phones[0].number}` + "</ul>" +
               "<ul>" + `Website: ` + `<a href="${doctor.data[i].practices[0].website}">` + `${doctor.data[i].practices[0].website}` + "</a>" +  "</ul>" +
               "<ul>" +"Accepting New Patients: No" + "</ul<");
-        }
-        foundCounter++;
-      }
-      if (foundCounter === 0){
-        $('.showCounter').text(foundCounter + ' found: NO DOCTOR MEET CRITERIA');
-      } else {
-        $('.showCounter').text(foundCounter + ' showing');
-      }
-    }, function(error) {
-      $('.search').text(error.message);
+            }
+            foundCounter++;
+          }
+          if (foundCounter === 0){
+            $('.showCounter').text(foundCounter + ' found: NO DOCTOR MEET CRITERIA');
+          } else {
+            $('.showCounter').text(foundCounter + ' showing');
+          }
+        }, function(error) {
+          $('.search').text(error.message);
+        });
+      });
+      $('#nextPage').click(function(){
+        page = page + 5;
+        console.log(page)
+        $('.search').text("");
+        $('#symptonSearch').click();
+      });
     });
-  });
-  $('#nextPage').click(function(){
-    page = page + 5;
-    console.log(page)
-    $('.search').text("");
-    $('#symptonSearch').click();
-  });
-  //
-  // $('#nameSearch').click(function(){
-  //   $('.search').text("");
-  //   let name = $('#name').val();
-  //   let doctorLookup = new DoctorLookup();
-  //   let namePromise = doctorLookup.findDocByName(name, page);
-  //
-  //   namePromise.then(function(response){
-  //     let doctor = JSON.parse(response);
-  //     let foundCounter = 0;
-  //     for (let i = 0; i < doctor.data.length; i++) {
-  //       $('.search').append("<li>" + `${doctor.data[i].profile.first_name}  ${doctor.data[i].profile.last_name}` + "</li>");
-  //       foundCounter++;
-  //     }
-  //     if (foundCounter === 0){
-  //       $('.showCounter').text(foundCounter + ' found');
-  //     } else {
-  //       $('.showCounter').text(foundCounter + ' showing');
-  //     }
-  //   }, function(error) {
-  //     $('.search').text(error.message);
-  //   });
-  // });
-  // $('#nextName').click(function(){
-  //   page = page + 5;
-  //   console.log(page)
-  //   $('.search').text("");
-  //   $('#nameSearch').click();
-  // });
-});
--- src/doctor.js
+++ src/doctor.js
@@ -1,26 +1,8 @@
 export class DoctorLookup{
-  findDocBySympton(sympton, page){
+  findDocBySympton(sympton, name, page){
     return new Promise(function(resolve, reject){
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${sympton}&location=wa-seattle&skip=${page}&limit=5&user_key=${process.env.exports.apiKey}`;
-
-
-      request.onload = function(){
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      }
-      request.open("GET", url, true);
-      request.send();
-    });
-  }
-
-  findDocByName(name, page){
-    return new Promise(function(resolve, reject){
-      let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=wa-seattle&skip=${page}&limit=3&user_key=${process.env.exports.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${sympton}&location=wa-seattle&skip=${page}&limit=5&user_key=${process.env.exports.apiKey}`;
 
 
       request.onload = function(){
